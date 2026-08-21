8532276f4958f269e516e523a6c99d92877a7e5a
pedro <pedbcrespo@gmail.com>
2026-08-21 09:20:44 -0300

    implementando controller

--- controller/establishment_controller.py
+++ controller/establishment_controller.py
@@ -1,2 +1,20 @@
 from flask import Blueprint, request, jsonify
 from configuration import db, BASE_URL
+from service.establishment_service import EstablishmentService
+from repository.establishment_repository import EstablishmentRepository
+
+service = EstablishmentService(EstablishmentRepository())
+
+establishment_bp = Blueprint('establishment', __name__, url_prefix=f'/{BASE_URL}/establishments')
+
+@establishment_bp.route('/register', methods=['POST'])
+def register():
+    pass
+
+@establishment_bp.route('/update', methods=['PUT'])
+def update():
+    pass
+
+@establishment_bp.route('/delete', methods=['DELETE'])
+def delete():
+    pass
